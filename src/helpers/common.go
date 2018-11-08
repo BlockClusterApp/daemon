9ce797753d2fcacaa8b26a26e645b8232f2fafb9
Jibin Mathews <jazzyarchitects@users.noreply.github.com>
2018-11-08 20:29:07 +0530

    Update daemon version

--- src/helpers/common.go
+++ src/helpers/common.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-const CURRENT_AGENT_VERSION = "1.0";
+const CURRENT_AGENT_VERSION = "1.2";
 
 func UnmarshalJson(input []byte) (map[string]interface{}, error) {
 	var result interface{}
